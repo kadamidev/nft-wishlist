770b0427dc097a1e943215fefa85bb09c7c90f42
Dev <kadamidev@users.noreply.github.com>
2022-02-18 21:52:24 +0700

    landing page complete for all viewports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
-import React from "react"
-import "./App.css"
+import React, { useState } from "react"
+import styles from "./App.module.scss"
+import logo from "./img/logo.svg"
 
 function App() {
-  return <div className="App"></div>
+  const [listId, setListId] = useState<string>("")
+
+  const handleJoin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    //query api for list id and redirect if successful
+  }
+
+  return (
+    <div className={styles.container}>
+      <img src={logo} alt="NFT Wishlist Logo" className={styles.logo} />
+
+      <div className={styles.menuContainer}>
+        <button className={styles.createListBtn}>CREATE NEW LIST</button>
+
+        <div className={styles.recentsContainer}>
+          <span>Recently Visited</span>
+          <ul>
+            <li>12345</li>
+            <li>67891</li>
+            <li>112134</li>
+          </ul>
+        </div>
+        <form className={styles.joinForm} onSubmit={(e) => handleJoin(e)}>
+          <input
+            type="text"
+            placeholder="enter list id"
+            value={listId}
+            onChange={(e) => setListId(e.target.value)}
+          />
+          <button type="submit">JOIN</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,17 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
+import List from "./List"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/list" element={<List />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 )
