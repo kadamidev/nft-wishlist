bb1040bf142301b2a0d115becf62a0016069c49e
Dev <kadamidev@users.noreply.github.com>
2022-03-07 00:56:52 +0700

    added functionality to copy code/link buttons in the navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import logo from "../../img/logo.svg"
 import styles from "./Navbar.module.scss"
@@ -9,6 +9,26 @@ interface Props {
 
 const Navbar: React.FC<Props> = ({ code }) => {
   const navigate = useNavigate()
+
+  const [copiedCode, setCopiedCode] = useState<boolean>(false)
+  const [copiedLink, setCopiedLink] = useState<boolean>(false)
+
+  function handleCopyCode() {
+    navigator.clipboard.writeText(code)
+    setCopiedCode(true)
+    setTimeout(() => {
+      setCopiedCode(false)
+    }, 3000)
+  }
+
+  function handleCopyLink() {
+    navigator.clipboard.writeText(window.location.href)
+    setCopiedLink(true)
+    setTimeout(() => {
+      setCopiedLink(false)
+    }, 3000)
+  }
+
   return (
     <nav className={styles.navContainer}>
       <img
@@ -19,8 +39,17 @@ const Navbar: React.FC<Props> = ({ code }) => {
       />
 
       <div className={styles.navRight}>
-        <button className={styles.code}>{code}</button>
-        <button className={styles.share}>SHARE LINK</button>
+        <button
+          className={
+            copiedCode ? [styles.code, styles.copied].join(" ") : styles.code
+          }
+          onClick={handleCopyCode}
+        >
+          {copiedCode ? "copied!" : code}
+        </button>
+        <button className={styles.share} onClick={handleCopyLink}>
+          {copiedLink ? "copied!" : "SHARE LINK"}
+        </button>
       </div>
     </nav>
   )
